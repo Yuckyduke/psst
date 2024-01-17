57a568a27c37ce0cba69058d9396b59caf7c98eb
Matt Brown <mrmatthewlewisbrown@gmail.com>
2024-01-16 18:11:53 -0800

    xCode

---
+++ Documents/psst/psst/psstApp.swift
@@ -0,0 +1,17 @@
+//
+//  psstApp.swift
+//  psst
+//
+//  Created by Matthew Brown on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct psstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

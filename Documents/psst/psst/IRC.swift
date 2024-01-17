aff957aacf3b6097ecfc9cf0eccbdc74271d77e6
Matt Brown <mrmatthewlewisbrown@gmail.com>
2024-01-16 20:42:54 -0800

    adding stuff

--- Documents/psst/psst/IRC.swift
+++ Documents/psst/psst/IRC.swift
@@ -6,12 +6,13 @@
 //
 
 import Foundation
-
-class User {
+let cheese = [Channel(id: "Poop")]
+ class User {
     var username: String
     var mode: Int
     var realname: String
     var away: Bool = false
+    var channelPrivs: [String]
     init(username: String, mode: Int, realname: String, away: Bool) {
         self.username = username
         self.mode = mode
@@ -24,15 +25,38 @@ class User {
     }
     
 }
+class Channel{
+    var id: String
+    var topic: String = "No Topic Set"
+    init(id: String) {
+        self.id = id
+    }
+    
+    func setTopic(topic: String){
+        self.topic = topic
+    }
+}
+
+func getChannel(id: String) -> Channel?{
+    for channel in cheese{
+        if channel.id == id{
+            return channel
+        }
+    }
+    return nil
+}
 
 enum Command {
     case pass (passwd: String)
     case nick (nickname: String)
-    case user (username: String, mode: Int, unused: String, realname: String)
+    case user (username: String, mode: Int, realname: String)
+    case join(channel: String, key: String?)
+    case part(channelList: [String], partMessage: String?)
+    case topic(channel: String, topic: String?)
 }
 
 enum Mode{
-    case a ()
+    case a
     case i
     case w
     case r
@@ -46,14 +70,31 @@ protocol IRC_Client{
 }
 
 struct Client: IRC_Client{
-    func message(_ command: Command) {
+    func message(_ command: Command, user: User) {
         switch command{
         case .pass(let passwd):
             print(passwd)
         case .nick(let nickname):
             print(nickname)
-        case .user:
-            print("Yo")
+        case .user(let username, let mode, let realname):
+            print(username + String(mode) + realname)
+            print("poop")
+        case .join(let channel, let key):
+            print(channel + key!)
+        case .part(let channelList, let partMessage):
+            for channel in channelList{
+                print(channel)
+            }
+            print(partMessage!)
+        case .topic(let channel, let topic?):
+            if user.channelPrivs.contains(channel){
+                if getChannel(id: channel) != nil {
+                    let topicChannel = getChannel(id: channel)
+                    if topic != nil{
+                        topicChannel.setTopic(topic: topic)
+                    }
+                }
+            }
         }
     }
 }

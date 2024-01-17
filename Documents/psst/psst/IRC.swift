d895582d99216b028ddd9c5d49cebff2537f486b
Matt Brown <mrmatthewlewisbrown@gmail.com>
2024-01-17 12:06:52 -0800

    added a parser

--- Documents/psst/psst/IRC.swift
+++ Documents/psst/psst/IRC.swift
@@ -4,10 +4,61 @@
 //
 //  Created by Matthew Brown on 1/16/24.
 //
+//message    =  [ ":" prefix SPACE ] command [ params ] crlf
+//prefix     =  servername / ( nickname [ [ "!" user ] "@" host ] )
+//command    =  1*letter / 3digit
+//params     =  *14( SPACE middle ) [ SPACE ":" trailing ]
+//           =/ 14( SPACE middle ) [ SPACE [ ":" ] trailing ]
+//
+//nospcrlfcl =  %x01-09 / %x0B-0C / %x0E-1F / %x21-39 / %x3B-FF
+//                ; any octet except NUL, CR, LF, " " and ":"
+//middle     =  nospcrlfcl *( ":" / nospcrlfcl )
+//trailing   =  *( ":" / " " / nospcrlfcl )
+//
+//SPACE      =  %x20        ; space character
+//crlf       =  %x0D %x0A   ; "carriage return" "linefeed"
 
 import Foundation
 let cheese = [Channel(id: "Poop")]
- class User {
+struct Message {
+    let prefix: String
+    let command: String
+    let args: [String]
+}
+
+func parseMsg(s: String) -> Message{
+    /*
+     takes a String from an IRC server and returns a Message
+     */
+    var buffer = s
+    var prefix = ""
+    var trailing: [String] = []
+    var args: [String]
+    var command: String
+    if buffer == ""{
+        print("Empty Message") // TODO: error handling
+    }
+    if buffer[buffer.startIndex] == ":"{
+        var split = buffer.split(separator: " ", maxSplits: 1).map {String($0)}
+        prefix = split[0]
+        buffer = split[1]
+    }
+    if let range = buffer.range(of: " :"){
+        if range.upperBound < buffer.endIndex{
+            var split = buffer.split(separator: " :", maxSplits: 1).map {String($0)}
+            buffer = split[0]
+            trailing.append(split[1])
+            var args = buffer.split(separator: " ").map {String($0)}
+            args.append(contentsOf: trailing)
+        }
+    }
+    else {
+        var args = buffer.split(separator: " ").map {String($0)}
+        var command = args.removeFirst()
+    }
+    return Message(prefix: prefix, command: command, args: args)
+}
+struct User {
     var username: String
     var mode: Int
     var realname: String
@@ -21,20 +72,20 @@ let cheese = [Channel(id: "Poop")]
         self.channelPrivs = []
     }
     
-    func goAway(){
-        self.away = true
+    mutating func goAway(){
+        away = true
     }
     
 }
-class Channel{
+struct Channel{
     var id: String
     var topic: String = "No Topic Set"
     init(id: String) {
         self.id = id
     }
     
-    func setTopic(topic: String){
-        self.topic = topic
+    mutating func setTopic(newTopic: String){
+        topic = newTopic
     }
 }
 
@@ -70,35 +121,35 @@ protocol IRC_Client{
     func message(_ command: Command, user: User)
 }
 
-struct Client: IRC_Client{
-    func message(_ command: Command, user: User) {
-        switch command{
-        case .pass(let passwd):
-            print(passwd)
-        case .nick(let nickname):
-            print(nickname)
-        case .user(let username, let mode, let realname):
-            print(username + String(mode) + realname)
-            print("poop")
-        case .join(let channel, let key):
-            print(channel + key!)
-        case .part(let channelList, let partMessage):
-            for channel in channelList{
-                print(channel)
-            }
-            print(partMessage!)
-        case .topic(let channel, let topic):
-            print(channel + topic!)
-            //            if user.channelPrivs.contains(channel){
-            //                if getChannel(id: channel) != nil {
-            //                    let topicChannel = getChannel(id: channel)
-            //                    if topic != nil{
-            //                        topicChannel.setTopic(topic: topic)
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-        }
-    }
-}
+//struct Client: IRC_Client{
+//    func message(_ command: Command, user: User) {
+//        switch command{
+//        case .pass(let passwd):
+//            print(passwd)
+//        case .nick(let nickname):
+//            print(nickname)
+//        case .user(let username, let mode, let realname):
+//            print(username + String(mode) + realname)
+//            print("poop")
+//        case .join(let channel, let key):
+//            print(channel + key!)
+//        case .part(let channelList, let partMessage):
+//            for channel in channelList{
+//                print(channel)
+//            }
+//            print(partMessage!)
+//        case .topic(let channel, let topic):
+//            print(channel + topic!)
+//            //            if user.channelPrivs.contains(channel){
+//            //                if getChannel(id: channel) != nil {
+//            //                    let topicChannel = getChannel(id: channel)
+//            //                    if topic != nil{
+//            //                        topicChannel.setTopic(topic: topic)
+//            //                    }
+//            //                }
+//            //            }
+//            //        }
+//            //    }
+//        }
+//    }
+//}
